362567f09337e27d4d7e25ae13bbbd388f10dae3
smartlotus <smartlotus1130@gmail.com>
2026-03-29 11:49:53 +0800

    sync site (exclude large build artifacts)

--- backups/card-assistant-new/20260322-183503-add-card/config.js
+++ backups/card-assistant-new/20260322-183503-add-card/config.js
@@ -2,15 +2,12 @@
   { title: "Lotus\u0027page", image: "./asset/image/cards/lotus-page-card.png", link: "https://lotusspagefinal.pages.dev/", alt: "Lotus\u0027page" },
   { title: "Lerobot", image: "./asset/image/cards/lerobot-card.png", link: "https://lerobot.pages.dev/", alt: "Lerobot" },
   { title: "庐山派", image: "./asset/image/cards/item-20260321112945-card.png", link: "https://lushanpai.pages.dev/", alt: "庐山派" },
-  { title: "偷偷地Wifi", image: "./asset/image/cards/wifi-card.jpg", link: "https://sushe-wifi.pages.dev/", alt: "偷偷地Wifi" },
-  { title: "Sountoz", image: "./asset/image/cards/sountoz-card.png", link: "https://sountoz.pages.dev/", alt: "Sountoz" },
   { title: "泰山派", image: "./asset/image/cards/item-20260321113014-card.jpg", link: "http://taishanpai.pages.dev/", alt: "泰山派" },
-  { title: "kindle_page", image: "./asset/image/cards/kindle-page-card.jpg", link: "https://kindle-page-web.pages.dev/", alt: "kindle_page" },
-  { title: "pickmark", image: "./asset/image/cards/pickmark-card.png", link: "https://pickmarksweb.pages.dev/", alt: "pickmark" },
   { title: "Pickplan", image: "./asset/image/cards/pickplan-card.jpg", link: "https://pickplanweb.pages.dev/", alt: "Pickplan" },
   { title: "Pickup", image: "./asset/image/cards/pickup-card.jpg", link: "https://pickupweb.pages.dev/", alt: "Pickup" },
   { title: "Pickbank", image: "./asset/image/cards/pickbank-card.jpg", link: "https://pickbankwebs.pages.dev/", alt: "Pickbank" },
   { title: "Mindstream", image: "./asset/image/cards/mindstream-card.png", link: "https://lotussmindstream.pages.dev/", alt: "Mindstream" },
-  { title: "Insight", image: "./asset/image/cards/insight-card.png", link: "https://lotussinsight.pages.dev/", alt: "Insight" }
+  { title: "Insight", image: "./asset/image/cards/insight-card.png", link: "https://lotussinsight.pages.dev/", alt: "Insight" },
+  { title: "pickmark", image: "./asset/image/cards/pickmark-card.png", link: "https://pickmarksweb.pages.dev/", alt: "pickmark" }
 ];
 
